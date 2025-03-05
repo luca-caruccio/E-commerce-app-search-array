7e35e338fb7a21908cd67400bb640171ddc4216f
Luca <lfcaruccio@gmail.com>
2025-03-05 14:28:01 -0800

    finsihed sorting mechanism

--- books.js
+++ books.js
@@ -1,33 +1,67 @@
-function renderBooks() {
-  const booksWrapper = document.querySelector('.books');
+function renderBooks(filter) {
+  const booksWrapper = document.querySelector(' .books');
 
   const books = getBooks();
 
-books.map(book => {
-  console.log(book)
-})
+    if (filter === `LOW_TO_HIGH`) {
+      
+      books.sort((a, b) => a.originalPrice - b.originalPrice);
+  }
+
+  else if (filter === `HIGH_TO_LOW`) {
+    books.sort((a, b) => b.originalPrice - a.originalPrice);
+  }
+
+  else if (filter === `RATING`) {
+    books.sort((a, b) => b.rating - a.rating);
+  }
 
 
-  booksWrapper.innerHTML = 
-  `<div class="book">
+const booksHtml = books.map((book) => {
+  return `<div class="book">
               <figure class="book__img--wrapper">
-                <img class="book__img" src="${books[0].url}" alt="">
+                <img class="book__img" src="${book.url}" alt="">
               </figure>
               <div class="book__title">
-              ${books[0].title}
-                Crack the Coding Interview
+              ${book.title}
               </div>
               <div class="book__ratings">
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star-half-alt"></i>
+                ${ratingsHTML(book.rating)}
               </div>
               <div class="book__price">
-                <span class="book__price--normal">$${books[0].originalPrice}</span> $${books[0].salePrice}
-              </div>
+                <span class=>$${book.originalPrice.toFixed(2)}</span> 
             </div>`
+})
+  .join("");
+
+
+
+booksWrapper.innerHTML = booksHtml;
+
+function ratingsHTML(rating) {
+  let ratingHTML = '';
+
+
+  for (let i = 0; i < Math.floor(rating); ++i) {
+    ratingHTML += `<i class="fas fa-star"></i>\n`
+  }
+
+if (!Number.isInteger(rating)) {
+  ratingHTML += `<i class="fas fa-star-half-alt"></i>\n`
+}
+return ratingHTML;
+}
+
+  //booksWrapper.innerHTML = 
+  
+
+}
+
+function filterBooks(event) {
+  
+  renderBooks(event.target.value);
+  
+  
 }
 
 setTimeout(() => {
